e7b14abc71c4b2d5aca76b8ed41733e7538f2adb
zyd-ustc <131000367+zyd-ustc@users.noreply.github.com>
2025-12-08 16:23:30 +0800

    Uncomment SDXL class and its methods

--- comfy/model_base.py
+++ comfy/model_base.py
@@ -471,30 +471,30 @@ class SD21UNCLIP(BaseModel):
 #         flat = mint.flatten(mint.cat(out)).unsqueeze(dim=0).repeat(clip_pooled.shape[0], 1)
 #         return mint.cat((clip_pooled.to(flat.device), flat), dim=1)
 
-# class SDXL(BaseModel):
-#     def __init__(self, model_config, model_type=ModelType.EPS, device=None):
-#         super().__init__(model_config, model_type, device=device)
-#         self.embedder = Timestep(256)
-#         self.noise_augmentor = CLIPEmbeddingNoiseAugmentation(**{"noise_schedule_config": {"timesteps": 1000, "beta_schedule": "squaredcos_cap_v2"}, "timestep_dim": 1280})
+class SDXL(BaseModel):
+    def __init__(self, model_config, model_type=ModelType.EPS, device=None):
+        super().__init__(model_config, model_type, device=device)
+        self.embedder = Timestep(256)
+        self.noise_augmentor = CLIPEmbeddingNoiseAugmentation(**{"noise_schedule_config": {"timesteps": 1000, "beta_schedule": "squaredcos_cap_v2"}, "timestep_dim": 1280})
 
-#     def encode_adm(self, **kwargs):
-#         clip_pooled = sdxl_pooled(kwargs, self.noise_augmentor)
-#         width = kwargs.get("width", 768)
-#         height = kwargs.get("height", 768)
-#         crop_w = kwargs.get("crop_w", 0)
-#         crop_h = kwargs.get("crop_h", 0)
-#         target_width = kwargs.get("target_width", width)
-#         target_height = kwargs.get("target_height", height)
+    def encode_adm(self, **kwargs):
+        clip_pooled = sdxl_pooled(kwargs, self.noise_augmentor)
+        width = kwargs.get("width", 768)
+        height = kwargs.get("height", 768)
+        crop_w = kwargs.get("crop_w", 0)
+        crop_h = kwargs.get("crop_h", 0)
+        target_width = kwargs.get("target_width", width)
+        target_height = kwargs.get("target_height", height)
 
-#         out = []
-#         out.append(self.embedder(mindspore.tensor([height])))
-#         out.append(self.embedder(mindspore.tensor([width])))
-#         out.append(self.embedder(mindspore.tensor([crop_h])))
-#         out.append(self.embedder(mindspore.tensor([crop_w])))
-#         out.append(self.embedder(mindspore.tensor([target_height])))
-#         out.append(self.embedder(mindspore.tensor([target_width])))
-#         flat = mint.flatten(mint.cat(out)).unsqueeze(dim=0).repeat(clip_pooled.shape[0], 1)
-#         return mint.cat((clip_pooled.to(flat.device), flat), dim=1)
+        out = []
+        out.append(self.embedder(mindspore.tensor([height])))
+        out.append(self.embedder(mindspore.tensor([width])))
+        out.append(self.embedder(mindspore.tensor([crop_h])))
+        out.append(self.embedder(mindspore.tensor([crop_w])))
+        out.append(self.embedder(mindspore.tensor([target_height])))
+        out.append(self.embedder(mindspore.tensor([target_width])))
+        flat = mint.flatten(mint.cat(out)).unsqueeze(dim=0).repeat(clip_pooled.shape[0], 1)
+        return mint.cat((clip_pooled.to(flat.device), flat), dim=1)
 
 
 # class SVD_img2vid(BaseModel):
