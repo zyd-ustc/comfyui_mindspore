70b7711ad3caf8c340e0f1537d1b63177eb87f51
zyd-ustc <131000367+zyd-ustc@users.noreply.github.com>
2025-12-09 10:29:54 +0800

    Uncomment CheckpointLoader class and methods

--- nodes.py
+++ nodes.py
@@ -540,21 +540,21 @@ class LoadLatent:
         return True
 
 
-# class CheckpointLoader:
-#     @classmethod
-#     def INPUT_TYPES(s):
-#         return {"required": { "config_name": (folder_paths.get_filename_list("configs"), ),
-#                               "ckpt_name": (folder_paths.get_filename_list("checkpoints"), )}}
-#     RETURN_TYPES = ("MODEL", "CLIP", "VAE")
-#     FUNCTION = "load_checkpoint"
+class CheckpointLoader:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "config_name": (folder_paths.get_filename_list("configs"), ),
+                              "ckpt_name": (folder_paths.get_filename_list("checkpoints"), )}}
+    RETURN_TYPES = ("MODEL", "CLIP", "VAE")
+    FUNCTION = "load_checkpoint"
 
-#     CATEGORY = "advanced/loaders"
-#     DEPRECATED = True
+    CATEGORY = "advanced/loaders"
+    DEPRECATED = True
 
-#     def load_checkpoint(self, config_name, ckpt_name):
-#         config_path = folder_paths.get_full_path("configs", config_name)
-#         ckpt_path = folder_paths.get_full_path_or_raise("checkpoints", ckpt_name)
-#         return comfy.sd.load_checkpoint(config_path, ckpt_path, output_vae=True, output_clip=True, embedding_directory=folder_paths.get_folder_paths("embeddings"))
+    def load_checkpoint(self, config_name, ckpt_name):
+        config_path = folder_paths.get_full_path("configs", config_name)
+        ckpt_path = folder_paths.get_full_path_or_raise("checkpoints", ckpt_name)
+        return comfy.sd.load_checkpoint(config_path, ckpt_path, output_vae=True, output_clip=True, embedding_directory=folder_paths.get_folder_paths("embeddings"))
 
 class CheckpointLoaderSimple:
     @classmethod
